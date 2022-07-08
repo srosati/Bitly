784448465277980f1cc6a741e84e7a0fe03edbed
srosati <rosatisantos@gmail.com>
2022-07-08 11:22:24 -0300

    swagger

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './swagger_output.json' assert { type: 'json' };
+
 import authRouter from './auth/index.js';
 import usersRouter from './users/index.js';
 import tagsRouter from './tags/index.js';
@@ -11,5 +14,7 @@ app.use(express.json());
 app.use('/users', usersRouter);
 app.use('/tags', tagsRouter);
 app.use('/auth', authRouter);
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(PORT, () => console.log('App listening on port', PORT));
